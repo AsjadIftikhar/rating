966644cee277a245b733901d15ff415a75b5fe23
kai <iftikharasjad@gmail.com>
2022-02-27 15:26:14 +0500

    view set

--- home/views.py
+++ home/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from authenticate.decorators import authenticated_user
 from django.contrib.auth import logout
+from home.helpers import *
 
 
 # Login Page Controller
@@ -14,3 +15,17 @@ def home(request):
 def log_out(request):
     logout(request)
     return redirect('/login')
+
+
+# Upload Files
+@authenticated_user
+def upload_files(request):
+    if request.method == 'POST':
+        # here you get the files needed
+        file = request.FILES['sentFile']
+        fileContent = pdf_to_string(file)
+
+        print(fileContent)
+
+    context = {}
+    return render(request, 'home/upload_files.html', context)
